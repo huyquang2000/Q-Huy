5b87e6300d4d58373adfa0d66c68c505725dc0eb
unknown <huyti28102000@gmail.com>
2022-03-14 15:26:49 +0700

    first commit

---
+++ login/src/service/LoginUser.js
@@ -0,0 +1,14 @@
+const userInfo = [
+    {
+        name: 'quanghuy',
+        password: 'huy123',
+        themeDefault: 'DARK',
+        language:'VI',
+    },
+    {
+        name: 'binh',
+        password: '123binh',
+        themeDefault: 'LIGHT',
+        language:'EN',
+    }
+]
